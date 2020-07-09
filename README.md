7958998aba831f9193737e5072c7ffbf8f58253d
VodBox <dillon@vodbox.io>
2020-07-09 15:38:32 +1200

    Initial commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# txt2sub.js - Text to Captions/Subtitle Conversion Library
+txt2sub takes raw text as a string, and converts to a valid caption/subtitle format that will be understood by many subtitle editors, or non-linear editors.
+
+## Supported Formats
+Currently the only format supported for conversion is to the SRT timed text format.
+
+## Simple Usage
+```html
+<script src="txt2sub.js"></script>
+<script>
+    var text = "Text can be on several lines\n"
+             + "Line breaks separate individual captions.\n"
+             + "\n"
+             + "Empty lines are ignored by default.\n"
+             + "Output will be valid SRT subtitles.";
+
+    txt2srt(text);
+    /*
+        1
+        00:00:00,000 --> 00:00:15,000
+        Text can be on several lines
+
+        2
+        00:00:15,000 --> 00:00:30,000
+        Line breaks separate individual captions.
+
+        3
+        00:00:30,000 --> 00:00:45,000
+        Empty lines are ignored by default.
+
+        4
+        00:00:45,000 --> 00:01:00,000
+        Output will be valid SRT subtitles.
+    */
+</script>
+```
+
+## Advanced Usage
+
+```javascript
+txt2srt(text, {
+    ignoreEmptyLines: true,
+    videoLength: 60
+});
+```
---
+++ txt2sub.js
@@ -0,0 +1,60 @@
+(function() {
+	var defaultValue = function(obj, val) {
+		if (obj !== undefined) return obj;
+		return val;
+	};
+
+	var defaultOptions = function(options) {
+		options = defaultValue(options, {});
+		options.videoLength = defaultValue(options.videoLength, 60);
+		options.ignoreEmptyLines = defaultValue(options.ignoreEmptyLines, true);
+		return options;
+	}
+
+	var leadingZero = function(num, len) {
+		var str = "" + num;
+
+		while (str.length < len) {
+			str = "0" + str;
+		}
+
+		return str;
+	};
+
+	var getTimeString = function(time) {
+		var hour = leadingZero(Math.floor(time / 3600), 2);
+		var min = leadingZero(Math.floor(time / 60) % 60, 2);
+		var sec = leadingZero(Math.floor(time) % 60, 2);
+
+		return hour + ":" + min + ":" + sec + ",000";
+	};
+
+	txt2srt = function(text, options) {
+		options = defaultOptions(options);
+
+		text = text.replace(/\r\n/gm, "\n");
+
+		if (options.ignoreEmptyLines)
+			text = text.replace(/^\n/gm, "");
+
+		var lines = text.split("\n");
+		var timeDelta = options.videoLength / lines.length;
+
+		var out = "";
+
+		for (var i = 0, l = lines.length; i < l; ++i) {
+			var time = i * timeDelta;
+			var nextTime = (i + 1) * timeDelta;
+			
+			out += (i + 1) + "\n";
+			out += getTimeString(time) + " --> " + getTimeString(nextTime) + "\n";
+			out += lines[i];
+
+			if (i < l - 1) {
+				out += "\n\n";
+			}
+		}
+
+		return out;
+	}
+})();
